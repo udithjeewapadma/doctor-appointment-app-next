690c940feb9007a67c77c3d67ccf94d7f4d18b8e
Jeewapadma <udithjeewapadma117@gmail.com>
2024-12-08 15:57:24 +0530

    create and style header component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className=" md:px-20">
+          <Header/>
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -4,10 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>
-        Doctor booking app
-        <Button>Click</Button>
-      </h2>
 
     </div>
   );
